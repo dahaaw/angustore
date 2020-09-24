ff3c9a9f11badccbbd891c9cb66545d24ed3cf65
dahaaw <dahaaw@gmail.com>
2020-09-24 21:23:00 +0700

    add order route

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ const logger = require('morgan');
 const cors = require('cors');
 const app = express();
 
-//Import Routes
-const productsRoute = require('./routes/products');
-const usersRoute = require('./routes/users');
 
-// Use Routes
-app.use('/api/products',productsRoute);
-app.use('api/users',usersRoute);
 
 
 app.use(cors({
@@ -25,5 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Import Routes
+const productsRoute = require('./routes/products');
+const ordersRoute = require('./routes/orders');
+
+// Use Routes
+app.use('/api/products',productsRoute);
+app.use('/api/orders',ordersRoute);
 
 module.exports = app;
